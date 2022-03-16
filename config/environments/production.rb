84ff98fb0417af04283fdce8c1ad7cbef0dcbdb0
Kevin Guerrero <guerrk83z@gmail.com>
2022-03-16 18:07:20 -0400

    websocket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://devmaster-kg.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://devmaster-kg.herokuapp.com', /http:\/\/devmaster-kg.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
